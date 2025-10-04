eac0785aa98f1e6dd5e0f49bdf59c30f287cde05
moha <m.ahmadi1600@gmail.com>
2025-10-04 14:36:16 +0330

    chore: add static directory with placeholder

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -145,9 +145,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-# STATICFILES_DIRS = [BASE_DIR / 'static/']
-# STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static/']
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = BASE_DIR / 'mediafiles/'
