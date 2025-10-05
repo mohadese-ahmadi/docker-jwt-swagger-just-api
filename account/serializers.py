cd1b4be615b7533fedfae250908e4199305b240e
moha <m.ahmadi1600@gmail.com>
2025-10-05 14:51:56 +0330

    testing the autho deploy

--- account/serializers.py
+++ account/serializers.py
@@ -9,6 +9,6 @@ class AuthorSerializer(serializers.ModelSerializer):
         model = Author
         fields = [
             'id', 'username', 'email', 'first_name',
-            'last_name', 'sex', 'is_superuser'
+            'last_name', 'sex', 'is_superuser', 'is_staff'
         ]
         read_only_fields = ['id', 'is_superuser']
